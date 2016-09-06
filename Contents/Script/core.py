7c8766d2abdc8559753ad6e9e5447f4d850ddb12
zhili.yan <zhili.yan@rayjoy.com>
2016-09-07 05:24:23 +0800

    delete workspace

--- Contents/Script/core.py
+++ Contents/Script/core.py
@@ -470,12 +470,12 @@ def buildGradle(channel):
 
 def deleteWorkspace(channel):
     channelNum = channel['channelNum']
-    workDir = file_operate.get_server_dir()+'/workspace/' + channelNum
+    workDir = file_operate.get_server_dir()+'/workspace/'
     workDir = file_operate.getFullPath(workDir)
-    gameIconDir = file_operate.get_server_dir()+'/workspace/icon/' + channelNum
-    gameIconDir = file_operate.getFullPath(gameIconDir)
+    # gameIconDir = file_operate.get_server_dir()+'/workspace/icon/' + channelNum
+    # gameIconDir = file_operate.getFullPath(gameIconDir)
     file_operate.delete_file_folder(workDir)
-    file_operate.delete_file_folder(gameIconDir)
+    # file_operate.delete_file_folder(gameIconDir)
 # 定义函数 8.4
 def inspectJDK():
     # print '<---inspectJDK LD_LIBRARY_PATH--> '+os.environ['LD_LIBRARY_PATH']
