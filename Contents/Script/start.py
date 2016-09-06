b1030644a51c0bd4dc21fbfcf78758aa8a7aa012
zhili.yan <zhili.yan@rayjoy.com>
2016-09-07 05:07:59 +0800

    push icon

--- Contents/Script/start.py
+++ Contents/Script/start.py
@@ -40,7 +40,7 @@ def start():
     ConfigParse.shareInstance().readUserConfig(0)
     taskManager.shareInstance().clearRecord()
     packThreadManager.shareInstance().clearRecord()
-    packThreadManager.shareInstance().setCurstrlistWorkDir(os.getcwd())
+    packThreadManager.shareInstance().setCurWorkDir(os.getcwd())
     # source = ConfigParse.shareInstance().getSource()
     #print '<---source-->'+source
     # game = ConfigParse.shareInstance().getCurrentGame()
