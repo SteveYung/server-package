5baca2b6292abf7908216c67801c00a55f5bd7c2
zhili.yan <zhili.yan@rayjoy.com>
2016-09-20 01:16:00 +0800

    test

--- Contents/Script/config.py
+++ Contents/Script/config.py
@@ -335,7 +335,7 @@ class ConfigParse(object):
             sdkItem = self.__userSDKConfigLs.get(dictTemp['idUserSDK'])
             if sdkItem != None:
                 sdkItem['param'].append(dictTemp)
-                print '<TTTTTEEEEESSSSTTTT>'+self.__userSDKConfigLs.get(dictTemp['idUserSDK']).get('value')
+                print '<TTTTTEEEEESSSSTTTT>'+self.__userSDKConfigLs.get(dictTemp['idUserSDK']).get('param').get('value')
 
         c.close()
 
