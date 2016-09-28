5a7014b9a436a7ae14130e574b084bf0d1deb20f
zhili.yan <zhili.yan@rayjoy.com>
2016-09-29 03:56:18 +0800

    m

--- Contents/Script/apk_operate.py
+++ Contents/Script/apk_operate.py
@@ -1069,7 +1069,7 @@ def doModifyAppName(decompileDir,newAppName):
     stringLsNode = stringRoot.findall('string')
     for stringNode in stringLsNode:
         if stringNode.attrib.get('name') is not None and stringNode.attrib['name'] == appName:
-            stringNode.text = gameName
+            stringNode.text = gameName.encode('utf-8')
 
     stringTree.write(stringsXml, 'UTF-8')
 
