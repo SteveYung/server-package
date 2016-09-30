b00a7a2accf77027fe48311589451aa3d73d4219
zhili.yan <zhili.yan@rayjoy.com>
2016-10-01 00:50:19 +0800

    m

--- Contents/python/readcfg.py
+++ Contents/python/readcfg.py
@@ -11,5 +11,15 @@ db_pwd = cf.get("mysqlconf", "password")
 
 print db_host, db_port, db_user, db_pwd
 
+s = cf.sections()
+print 'section:', s
+
+o = cf.options("dbconf")
+print 'options:', o
+
+v = cf.items("mysqlconf")
+print 'db:', v
+
+
 # cf.set("mysqlconf", "db_pass", "123456")
 # cf.write(open("config_file_path", "w"))
