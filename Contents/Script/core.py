5dfeee15b711a92fe9595a102741c034d6bb1cc3
zhili.yan <zhili.yan@rayjoy.com>
2016-11-01 16:54:25 +0800

    工作环境 idchannel 替换 渠道号

--- Contents/Script/core.py
+++ Contents/Script/core.py
@@ -55,7 +55,7 @@ def main(channel):
             return
         taskManager.shareInstance().notify(idChannel, 10)
         # file_operate.execFormatCmd('chmod -R 777 %s' % (file_operate.get_server_dir()+'/workspace/'))
-        workDir = file_operate.get_server_dir()+'/workspace/' + channelNum
+        workDir = file_operate.get_server_dir()+'/workspace/' + idChannel
         workDir = file_operate.getFullPath(workDir)
         file_operate.delete_file_folder(workDir)
         if not os.path.exists(source):
@@ -473,8 +473,8 @@ def buildGradle(channel):
 """
 
 def deleteWorkspace(channel):
-    channelNum = channel['channelNum']
-    workDir = file_operate.get_server_dir()+'/workspace/%s' %(channelNum)
+    idChannel = channel['idChannel']
+    workDir = file_operate.get_server_dir()+'/workspace/%s' %(idChannel)
     workDir = file_operate.getFullPath(workDir)
     file_operate.delete_file_folder(workDir)
 # 定义函数 8.4
