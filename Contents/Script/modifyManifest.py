c969c8cc76705ba3d27f644c5b4c1c8e6047ad7b
zhili.yan <zhili.yan@rayjoy.com>
2016-11-18 02:54:23 +0800

    修复写入时的bug

--- Contents/Script/modifyManifest.py
+++ Contents/Script/modifyManifest.py
@@ -24,13 +24,16 @@ def doModify(manifestFile, sourceFile, root):
     f.close()
     appCfgNode = sourceRoot.find('applicationCfg')
     if appCfgNode is not None and len(appCfgNode) > 0:
-        appKeyWord = appCfgNode.get('keyword')
-        if appKeyWord != None and len(appKeyWord) > 0:
-            keyIndex = targetContent.find(appKeyWord)
-            if -1 == keyIndex:
-                bRet = True
-                for node in list(appCfgNode):
+        for node in list(appCfgNode):
+            key = '{' + androidNS + '}name'
+            nodeValue = node.get(key)
+            if nodeValue != None and len(nodeValue) > 0:
+                attrIndex = targetContent.find(nodeValue)
+                if -1 == attrIndex:
+                    bRet = True
                     root.find('application').append(node)
+                else:
+                    print '===========>SAME NOTE:'+nodeValue
 
     perCfgNode = sourceRoot.find('permissionCfg')
     if perCfgNode is not None and len(perCfgNode) > 0:
@@ -38,10 +41,8 @@ def doModify(manifestFile, sourceFile, root):
             key = '{' + androidNS + '}name'
             perAttr = oneNode.get(key)
             if perAttr != None and len(perAttr) > 0:
-                attrIndex = targetContent.find(perAttr)
-                if -1 == attrIndex:
-                    bRet = True
-                    root.append(oneNode)
+                bRet = True
+                root.append(oneNode)
 
     return bRet
 
@@ -64,6 +65,24 @@ def modify(manifestFile, sourceCfgFile, pluginName, usrSDKConfig):
     targetRoot = targetTree.getroot()
     haveChanged = doModify(manifestFile, sourceXml, targetRoot)
     if haveChanged:
+        keyname = '{' + androidNS + '}name'
+        usesPermissionNodes = targetRoot.findall('uses-permission')
+        usesPermissionNodes2 = []
+
+        for note in usesPermissionNodes:
+            keyValue = note.get(keyname)
+            deleteFlag = False
+            addFlag = True
+            for subnote in usesPermissionNodes2:
+                subkeyValue = subnote.get(keyname)
+                if(keyValue == subkeyValue):
+                    deleteFlag = True
+                    addFlag = False
+            if addFlag:
+                usesPermissionNodes2.append(note)
+            if deleteFlag:
+                print "REMOVE-SAME-uses-permission->"+keyValue
+                targetRoot.remove(note)
         file_operate.printf('Modify AndroidManifest.xml for plugin ' + pluginName)
         targetTree.write(manifestFile, 'UTF-8')
 
