2e59ae89d881f380b19ad0410242d38da21740c6
zhili.yan <zhili.yan@rayjoy.com>
2016-12-31 05:22:27 +0800

    split

--- Contents/Script/error_operate.py
+++ Contents/Script/error_operate.py
@@ -7,10 +7,13 @@ import file_operate
 import core
 
 def error(code):
-    idChannel = int(threading.currentThread().getName())
-    taskManager.shareInstance().notify(idChannel, 100 + code)
-    print '<---error code---> %d' %(code)
-    file_operate.printf('%s Failed at code %s!' % (idChannel, -100 - code))
+    try:
+        idChannel = int(threading.currentThread().getName())
+        taskManager.shareInstance().notify(idChannel, 100 + code)
+        print '<---error code---> %d' %(code)
+        file_operate.printf('%s Failed at code %s!' % (idChannel, -100 - code))
+    except:
+        print 'error code:'+code
 #+++ okay decompyling rsdk1.4/Script/error_operate.pyc 
 # decompiled 1 files: 1 okay, 0 failed, 0 verify failed
 # 2015.01.17 10:32:30 CST
